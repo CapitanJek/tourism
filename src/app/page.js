069eb5cccaf97e6d075f231aa885bbeec31bef91
nikkelliaa <niyaraemelbekova242@gmail.com>
2024-09-11 15:30:38 +0300

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer/Footer";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Container>
                 Hello world
             </Container>
+            <Footer/>
         </>
     );
 };
